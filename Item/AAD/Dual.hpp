44f3c4e38aaf3cef73a4a7d5ae8b817827b05c74
Yasserini <ahmedyasser44299@gmail.com>
2021-04-01 13:44:51 +0100

    Automatic Differentiation (AD)
    Utilizes simplistic forward pass Automatic Differentiation(AD), based on dual numbers, to calculate the derivatives with the same complexity as doing a singular pass through the desired function.

---
+++ Item/AAD/Dual.hpp
@@ -0,0 +1,45 @@
+//
+//  DualNumbers.hpp
+//  AAD
+//
+//  Created by Ahmed Yasser on 3/31/21.
+//
+
+#ifndef Dual_hpp
+#define Dual_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+using std::ostream;
+
+struct Dual {
+
+    Dual();
+    Dual(double r);
+    Dual(double r, double d);
+    Dual(const Dual &argument);
+    ~Dual() {};
+
+    Dual operator=(const Dual &argument);
+    Dual operator+(Dual argument) const;
+    Dual operator-(Dual argument) const;
+    Dual operator*(Dual argument) const;
+    Dual operator++(int notUsed);
+    Dual operator--(int notUsed);
+    Dual operator++();
+    Dual operator--();
+    
+    friend ostream& operator<<(ostream& os, const Dual& argument);
+    
+    Dual log(const Dual &dual_number);
+    Dual exp(const Dual &dual_number);
+    Dual sin(const Dual &dual_number);
+    Dual cos(const Dual &dual_number);
+    Dual sigmoid(const Dual &dual_number);
+    
+    double real;
+    double dual;
+};
+
+#endif /* Dual_hpp */
