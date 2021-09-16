1889a0ab7b612db4c6aa2c393733437d5dc446b1
HNASH <hussam.nashar7@gmail.com>
2021-09-16 23:55:09 +0200

    -

--- Item/Swap/InterestSwap.cpp
+++
@@ -1,7 +0,0 @@
-#include "InterestSwap.hpp"
-
-string InterestSwap::INTSWAP_PARAM_NAMES[5] = { "Name: ", "Payment Frequency: ", "Time to Expiry (Yrs): ", "Fixed Annual Interest Rate (%): ", "Spread (%): " };
-void InterestSwap::calculateSpot()
-{
-	int periods = couponFreq * timeToMaturity;
-}
